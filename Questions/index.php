e60ef20f4d4a5189c267cfdaae9371a9dd741281
sedrickotolo <sedricksedu2@gmail.com>
2023-08-06 14:38:42 +0300

    updtea

--- Questions/index.php
+++ Questions/index.php
@@ -5,7 +5,7 @@ include("config.php");
 <html>
 <head>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <title>Questions</title>
+    <title>Questions for Standout</title>
 </head>
 <body>
 <div class="container">
